1cc760e0ecf4a9fbc42e9cd09cf4d14b64fba903
jianl9 <jianliamos@gmail.com>
2018-02-22 02:04:10 -0800

    Update main.cpp

--- Minesweeper_Cpp/main.cpp
+++ Minesweeper_Cpp/main.cpp
@@ -1,4 +1,14 @@
-#include <iostream>
+//
+// FILE:        Main.cpp
+//
+// Created by Jian Li  on 2/19/18.
+//
+// DESCRIPTION: This file is the entry point for the program.
+// Right now, it only in charge of launching board generation
+// All tiles are set to uncover for visibility; set to false to display "."
+//
+
+
 #include "board.h"
 int main() {
     Board board;
