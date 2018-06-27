855693c3136e0b5546da43a5cdc993e4fc6c4245
Justin Chung <justinchung@Justins-MacBook-Pro-3.local>
2018-06-26 20:19:36 -0700

    Now updated WorldGenerator.py

--- WorldGenerator/WorldGenerator.py
+++ WorldGenerator/WorldGenerator.py
@@ -6,10 +6,20 @@
 # DESCRIPTION:	This file generates Worlds. The generator can be used to
 #				create any number of worlds.
 #
-# NOTES: 		- Make sure to specify a number for difficulty:
-#					1 --> Beginner
-#					2 --> Intermediate
-#					3 --> Expert
+# NOTES: 		- "numWorlds" = number of world files to create
+#				- "baseFileName" = the prefix of the file name, which
+#								   is then followed by a number
+#				- "rowDimension" = the number of rows
+#				- "colDimension" = the number of columns
+#				- "numMines" = the number of mines
+#
+#				- You can only generate worlds of the same dimensions
+#				  and same number of mines. In order to generate worlds
+#				  of other dimensions/ # of mines you have to run the
+#				  generator again with different arguments.
+#
+#				- The minimum row and column dimension is 4.
+#				- The minimum number of mines is 1.
 #				
 #				- DO NOT MAKE CHANGES TO THIS FILE.
 # ==============================CS-199==================================
@@ -19,36 +29,28 @@ import os
 import argparse
 
 
-def generateWorlds(numWorlds: int, difficulty: int, baseFileName: "string") -> None:
+def generateWorlds(numWorlds: int, baseFileName: "string", rowDimension: int, colDimension: int, numMines: int) -> None:
 	""" Generates N random worlds of a specified difficulty """
 	""" Each generated world will be a text file with baseFileName followed by a number """
 	for i in range(1, numWorlds+1):
-		createWorldFile(difficulty, baseFileName+str(i))
+		createWorldFile(baseFileName+str(i), rowDimension, colDimension, numMines)
 
 
-def createWorldFile(difficulty: int, filename: "string") -> None:
+def createWorldFile(filename: "string", rowDimension: int, colDimension: int, numMines: int) -> None:
 	""" Create a single Minesweeper world file """
 	print("Creating world " + filename + "...")
 	
 	directory_name = os.path.abspath("Worlds")
 	file_path = os.path.join(directory_name, filename+".txt")
 
-	# Set difficulty
-	if difficulty == 1:
-		nRows, nCols = 8, 8
-		nMines = 10
-	elif difficulty == 2:
-		nRows, nCols = 16, 16
-		nMines = 40
-	elif difficulty == 3:
-		nRows, nCols = 16, 30
-		nMines = 99
+	nRows = rowDimension
+	nCols = colDimension
+	nMines = numMines
 
 	# Randomly set the starting tile
 	startX = __randomInt(nCols+1)
 	startY = __randomInt(nRows+1)
 	startingPatch = [(startX, startY)]
-
 	# Surrounding tiles can't have a mine
 	for coord in [(-1, 1), (0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1), (-1, 0)]:
 		if __isInBounds(startX+coord[0], startY+coord[1], nRows, nCols):
@@ -58,8 +60,8 @@ def createWorldFile(difficulty: int, filename: "string") -> None:
 	mineCoords = []
 	currentMines = 0
 	while currentMines < nMines:
-		x = __randomInt(nCols)
-		y = __randomInt(nRows)
+		x = __randomInt(nCols+1)
+		y = __randomInt(nRows+1)
 		if (x, y) not in startingPatch and (x, y) not in mineCoords:
 			mineCoords.append((x, y))
 			currentMines += 1
@@ -77,7 +79,7 @@ def createWorldFile(difficulty: int, filename: "string") -> None:
 					if (x, y) in mineCoords:
 						file.write("1 ")
 					elif (x, y) in startingPatch:
-						file.write("x ")
+						file.write("0 ")
 					else:
 						file.write("0 ")
 				file.write("\n")
@@ -101,16 +103,23 @@ def main():
 
 	# Parse options																													# Help is default
 	parser.add_argument("numFiles", help="Number of world files to create", action="store", type=int)								# Number of files
-	parser.add_argument("difficulty", help="Beginner=1, Intermediate=2, Expert=3", action="store", type=int, choices=[1, 2, 3])		# Difficulty
 	parser.add_argument("filename", help="Base filename", action="store")															# Base filename
+	parser.add_argument("rowDimension", help="Number of rows", action="store", type=int)
+	parser.add_argument("colDimension", help="Number of columns", action="store", type=int)
+	parser.add_argument("numMines", help="Number of mines", action="store", type=int)
 
 	args = parser.parse_args()
 
 	numFiles = args.numFiles
-	difficulty = args.difficulty
 	filename = args.filename
+	rowDimension = args.rowDimension
+	colDimension = args.colDimension
+	numMines = args.numMines
 	
-	generateWorlds(numFiles, difficulty, filename)
+	if (rowDimension >= 4 and colDimension >= 4 and (numMines <= rowDimension*colDimension - 9) and numMines >= 1):
+		generateWorlds(numFiles, filename, rowDimension, colDimension, numMines)
+	else:
+		print("ERROR: Could not generate worlds! \n\trowDimension >= 4, colDimension >= 4, 1 <= numMines <= (rowDimension*colDimension - 9)")
 
 
 if __name__ == "__main__":
